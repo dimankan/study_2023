6cf2f8d1eede87f5a2fcf6df30edb43882ca173b
VorobyovDV <vorobyovdv@gnivc.ru>
2023-03-08 19:40:00 +0300

    создал пустой проект asp.net 5 mvc

---
+++ StudyMvcAspNet5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudyMvcAspNet5.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
